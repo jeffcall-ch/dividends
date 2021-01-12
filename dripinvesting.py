9ba8c92fe635a310fa2f5bf936435c6d67daa591
Jeff Call <jeff.call.ch@gmail.com>
2021-01-12 16:38:26 +0100

    DGR condition working

--- dripinvesting.py
+++ dripinvesting.py
@@ -5,6 +5,7 @@ import pathlib
 import os
 import shutil
 import pandas as pd
+import logging
 
 # path definitions
 python_repo_home_folder = pathlib.Path.home() / "Python" / "Python_repos" / "dividends"
@@ -18,7 +19,9 @@ proxies = {
 }
 
 # get the time from response
-response = requests.get(url, proxies=proxies)
+# response = requests.get(url, proxies=proxies)
+response = requests.get(url)
+
 url_time = response.headers['last-modified']
 url_date = parsedate(url_time)
 
@@ -39,19 +42,28 @@ listOfFiles = getListOfFiles(dripinvesting_folder.as_posix())
 
 # if there is only 1 xls file in the dividends folder AND the url_date is newer, than the file_date
 # then download new file
-file_time_of_latest_downloaded_xls = datetime.datetime.fromtimestamp(os.path.getmtime(listOfFiles[0])).astimezone()
-if (len(listOfFiles) == 1 & (url_date > file_time_of_latest_downloaded_xls)):
-    # move obsolete file to the "obsolete folder"
-    path_of_file = pathlib.Path(listOfFiles[0])
-    destination_path_of_file = path_of_file.parent / "obsolete" / path_of_file.name
-    shutil.move(path_of_file, destination_path_of_file)
 
+if len(listOfFiles) == 1:
+    file_time_of_latest_downloaded_xls = datetime.datetime.fromtimestamp(os.path.getmtime(listOfFiles[0])).astimezone()
+    if url_date > file_time_of_latest_downloaded_xls:
+        # move obsolete file to the "obsolete folder"
+        path_of_file = pathlib.Path(listOfFiles[0])
+        destination_path_of_file = path_of_file.parent / "obsolete" / path_of_file.name
+        shutil.move(path_of_file, destination_path_of_file)
+
+        # donwload new file
+        with open(dripinvesting_filepath.as_posix(), 'wb') as output:
+            output.write(response.content)
+        logging.info("New file downloaded, old file moved to obsolete.")
+    else:
+        logging.info("No file downloaded. Online file is not newer, than in the one in the folder.")
+        logging.info("url_date:" + str(url_date))
+        logging.info("file_time: " + str(file_time_of_latest_downloaded_xls))
+else:
+    logging.info("No files were present in the folder. New file is downloaded.")
     # donwload new file
     with open(dripinvesting_filepath.as_posix(), 'wb') as output:
         output.write(response.content)
-else:
-    print("New dripinvesting file downloaded.")
-    print("url_date:" + str(url_date))
-    print("file_time: " + str(file_time_of_latest_downloaded_xls))
+    logging.info("New file downloaded, old file moved to obsolete.")
 
 
--- pandas_play.py
+++ pandas_play.py
@@ -20,14 +20,19 @@ listOfFiles = getListOfFiles(dripinvesting_folder.as_posix())
 
 # read the excel file
 # combine rows 4-5 to be the header. By doing this the real pandas df will start from the header (and you don't need to drop rows..)
-df = pd.read_excel(listOfFiles[0], sheet_name='All CCC', header=[4,5]) 
+# note that in case the top cell is empty, then it will use the closest top cell value to the left.
+# EXAMPLE: "TickerSymbol" column (col "B" in excel) name is correct. But the next column is empty top cell and "Sector" bottom cell (col "C" in excel). 
+#           Now it will be "TickerSector"
+df = pd.read_excel(listOfFiles[0], sheet_name='All CCC', header=[4,5])
+
+# flatten the multi index header: https://stackoverflow.com/questions/41931332/how-do-i-flatten-a-hierarchical-column-index-in-a-pandas-dataframe
+df.columns = df.columns.map(lambda x: ''.join([*map(str, x)]))
 
 # drop last rows of summary where not individual companies are shown but sector performance
-df = df.dropna(subset=['Sector'])
+df = df.dropna(subset=['TickerSector'])
 
-# list(df.columns.values)
-
-# print(df.columns.tolist())
+# select only rows where DGR is not showing a decreasing trend
+df = df[(df['DGR1-yr']>=df['DGR3-yr']) & (df['DGR3-yr']>=df['DGR5-yr']) & (df['DGR5-yr']>=df['DGR10-yr'])]
 
 
 print(df)
